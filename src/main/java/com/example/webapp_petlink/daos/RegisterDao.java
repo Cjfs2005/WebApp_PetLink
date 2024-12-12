0d1770f267a17dcd25682220cffa18274b267f1a
Tony F <a20223209@pucp.edu.pe>
2024-12-11 19:57:35 -0500

    Avance en el registro de usuarios

---
+++ src/main/java/com/example/webapp_petlink/daos/RegisterDao.java
@@ -0,0 +1,23 @@
+package com.example.webapp_petlink.daos;
+
+import com.example.webapp_petlink.beans.DenunciaMaltratoAnimal;
+import com.example.webapp_petlink.beans.Estado;
+import com.example.webapp_petlink.beans.Usuario;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterDao extends DaoBase{
+
+    //Valida que no exista cuenta con el mismo correo o el mismo DNI
+
+
+    //Crea nuevo usuario y contrasenia temporal
+
+    //Crea nuevo albergue y contrasenia temporal
+
+    //Actualiza la contrasenia del usuario/Albergue
+
+
+}
